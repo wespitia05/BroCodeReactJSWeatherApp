41969575337f400d23d8e719ab5588ca27f32960
william <espiw@farmingdale.edu>
2026-02-04 19:47:49 -0500

    html elements complete (for now with placeholders)

--- my-react-weather-app/src/WeatherApp.jsx
+++ my-react-weather-app/src/WeatherApp.jsx
@@ -1,6 +1,24 @@
+import React, {useState} from "react";
+
+// our open weather api key
+const API_KEY = "0b27dee62b5909ebbbf9788a2e85e2b2";
+
 function WeatherApp() {
     return(
-        <p>hello</p>
+        <div className="weather-container">
+            <form className="weather-form">
+                <input type="text" className="cityInput" placeholder="Enter City"></input>
+                <button type="submit">Get Weather</button>
+            </form>
+            <div className="weather-card">
+                <h1 className="cityDisplay">Miami</h1>
+                <p className="tempDisplay">90°</p>
+                <p className="humidityDisplay">Humidity: 75%</p>
+                <p className="descDisplay">Clear Skies</p>
+                <p className="weatherEmoji">☀️</p>
+                <p className="errorDisplay">Please Enter A City</p>
+            </div>
+        </div>
     );
 }
 
