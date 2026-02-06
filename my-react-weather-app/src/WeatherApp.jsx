92691724c8bcac69059784c6d4924a4ce9e0704a
william <espiw@farmingdale.edu>
2026-02-05 22:07:41 -0500

    wind direction function complete

--- my-react-weather-app/src/WeatherApp.jsx
+++ my-react-weather-app/src/WeatherApp.jsx
@@ -126,8 +126,34 @@ function WeatherApp() {
     }
 
     // this function will determine the direction the wind is blowing based on degree
-    function getWindDirection() {
-
+    function getWindDirection(deg) {
+        if (deg >= 337.5 || deg < 22.5) {
+            return "↑";
+        }
+        else if (deg >= 22.5 && deg < 67.5) {
+            return "↗";
+        }
+        else if (deg >= 67.5 && deg < 112.5) {
+            return "→";
+        }
+        else if (deg >= 112.5 && deg < 157.5) {
+            return "↘";
+        }
+        else if (deg >= 157.5 && deg < 202.5) {
+            return "↓";
+        }
+        else if (deg >= 202.5 && deg < 247.5) {
+            return "↙";
+        }
+        else if (deg >= 247.5 && deg < 292.5) {
+            return "←";
+        }
+        else if (deg >= 292.5 && deg < 337.5) {
+            return "↖";
+        }
+        else {
+            return "•";
+        }
     }
 
     // this function will display any potential error messages
